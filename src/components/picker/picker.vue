19f0a398642bc314e11c7ab191d603f140c1249e
daimeng <daimeng@users.noreply.github.com>
2019-01-16 18:06:02 +0800

    update

--- src/components/picker/picker.vue
+++ src/components/picker/picker.vue
@@ -1,63 +1,107 @@
 <template>
   <div class="container">
-    <!-- <div class="picker-border" ref="wrapper">
-      <ul class="slider">
-        <li>aaaa</li>
-        <li>aaaa</li>
-        <li>aaaa</li>
-        <li>aaaa</li>
-        <li>aaaa</li>
-        <li>aaaa</li>
-      </ul>
-    </div> -->
-    <mo-bscroll class="mo-picker-border">
-      <ul class="mo-picker-slider">
-        <li>aaaa</li>
-        <li>aaaa</li>
-        <li>aaaa</li>
-        <li>aaaa</li>
-        <li>aaaa</li>
-        <li>aaaa</li>
-      </ul>
-    </mo-bscroll>
+    <div class="mask-top"></div>
+    <div class="picker-container">
+      <div class="wheel-wrapper" 
+      v-for="(item,index) in data" 
+      :key="index" 
+      ref="wrapper">
+        <ul class="wheel-scroll">
+          <li v-for="(item, index) in item"
+          class="wheel-item" 
+          :key="index"
+          >{{item.value}}</li>
+        </ul>
+      </div>
+    </div>
+    <div class="mask-bottom"></div>
   </div>
 </template>
 
 <script>
-import MoBscroll from '../bscroll/bscroll.vue'
+import BScroll from 'better-scroll'
 
 export default {
   name: 'MoPicker',
 
-  components: {
-    MoBscroll
-  },
-
   props: {
-    height: {
-      type: String,
-      defalut() {
-        return '20px'//20px common
-      }
-    }
+    data: {
+      type: Array,
+      default() {
+        return []
+      },
+    },
+    selectedIndex: {
+      type: Array,
+      default() {
+        return 0
+      },
+    },
   },
 
   data () {
     return {
+      wheels: [],
+      pickerValue: [],
+      pickerText: [],
+      pickerIndex: this.selectedIndex || []
     }
   },
 
   methods: {
-  },
-
-  computed: {
+    getPickerdata() {
+      this.data.forEach((wheelData,index) => {
+        this.pickerValue[index] = wheelData[this.pickerIndex[index]].value
+        this.pickerText[index] = wheelData[this.pickerIndex[index]].text
+      })
+    },
+    genDispatchData(wheel, index) {
+      this.pickerIndex[index] = wheel.getSelectedIndex()
+      this.getPickerdata()
+    },
+    getPickerIndex(wheel, index) {
+      this.pickerIndex[index] = wheel.getSelectedIndex()
+    },
+    getWheels() {
+      if (!this.$refs.wrapper) {
+        return
+      }
+      this.$refs.wrapper.forEach((wheelWrapper, index) => {
+        const wheel = new BScroll(wheelWrapper, {
+          wheel: {
+            selectedIndex: this.selectedIndex[index],
+            wheelWrapperClass: 'wheel-scroll',
+            wheelItemClass: 'wheel-item'
+          },
+          probeType: 3,
+        })
+        this.wheels[index] = wheel
+        this.genDispatchData(wheel, index)
+        wheel.on('scrollEnd',() => {
+          let oldIndex = this.pickerIndex[index]
+          this.genDispatchData(wheel, index)
+          if(oldIndex != this.pickerIndex[index]){
+            this.$emit('change',this.pickerValue,this.pickerIndex,this.pickerText)
+          }
+          this.$emit('select',this.pickerValue,this.pickerIndex,this.pickerText)
+        })
+      })
+    },
   },
 
   mounted() {
     this.$nextTick(() => {
-        // this.scroll = new Bscroll(this.$refs.wrapper, {})
-      })
-  }
+      this.getWheels()
+      this.$emit('select',this.pickerValue,this.pickerIndex,this.pickerText)
+    })
+  },
+
+  beforeDestroy() {
+    this.wheels && this.wheels.forEach((wheel) => {
+      wheel.destroy()
+    })
+    this.wheels = null
+  },
 }
 </script>
 
@@ -68,16 +112,42 @@ export default {
     align-items center
     overflow hidden
     position absolute
-    top 100px
-    height 200px
+    height 150px  //Easily modified
     width 100%
-    background #eeeeee
-    .mo-picker-border
+    border 1px solid #eeeeee  //common
+    .mask-top, .mask-bottom
+      z-index 10
       width 100%
-      height 20px
-      border 1px solid red
-    .mo-picker-border
+      height 60px   //modified
+      pointer-events none
+    .mask-top
+      border-bottom 1px solid #eeeeee //common
+      position absolute
+      top 0
+      background linear-gradient(to top, rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.8))
+    .mask-bottom
+      border-top 1px solid #eeeeee  //common
+      position absolute
+      bottom 0
+      background linear-gradient(to bottom, rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.8))
+    .picker-container
+      display flex
+      justify-content center
       width 100%
-      height 20px
-      border 1px solid red
+      height 100%
+      .wheel-wrapper
+        width 100%
+        height 100%
+        .wheel-scroll
+          width 100%
+          list-style none
+          margin-top 60px    //modified
+          .wheel-item
+            width 100%
+            display flex
+            justify-content center
+            align-items center
+            font-size 20px  //modified
+            height 30px     //modified
+    //wheel-item:height = container:height - (mask-top:height + mask-top:height)
 </style>
--- src/components/picker/test.vue
+++ src/components/picker/test.vue
@@ -1,10 +1,18 @@
 <template>
   <div >
-    <mo-picker></mo-picker>
+    <mo-picker
+    v-show="active"
+    :data="column"
+    :selectedIndex= "[1, 2]"
+    @select="getPickeData"
+    @change="getPickeData"
+    ></mo-picker>
+    <button class="button" @click="handleClick">{{active}}</button>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 import MoPicker from './picker.vue'
 export default {
   name: '',
@@ -18,17 +26,56 @@ export default {
 
   data () {
     return {
+      column: [
+        [{text: 'a',value: 'a'},{text: 'b',value: 'b'},{text: 'c',value: 'c'},{text: 'd',value: 'd'}],
+        [{text: 'a',value: 'a'},{text: 'e',value: 'e'},{text: 'f',value: 'f'},{text: 'g',value: 'g'}]
+      ],
+      active: true
     }
   },
 
   methods: {
+    getPickeData(array) {
+      console.log(array)
+    },
+    // handleClick() {
+    //   this.column = [
+    //     [{text: 'b',value: 'b'},{text: 'c',value: 'c'},{text: 'd',value: 'd'}],
+    //     // [{text: 'a',value: 'a'},{text: 'e',value: 'e'},{text: 'f',value: 'f'},{text: 'g',value: 'g'}]
+    //   ]
+    // }
+    handleClick() {
+      if(this.active)
+        this.active = false
+      else
+        this.active = true
+    }
   },
 
   computed: {
   },
+
+  mounted() {
+    // console.log(this.column)
+    this.$nextTick(() => {
+        // let bscrollDom = this.$refs.bscroll;
+        // this.aBScroll = new BScroll(bscrollDom,{
+        //   // click: true
+        // })
+    })   
+}
+
 }
 </script>
 
 <style scoped lang="stylus">
+  .bscroll{
+    width: 100%;
+    height: 50px;
+    overflow: hidden;
+}
+  .button
+    position absolute
+    top 200px
 
 </style>
